0f9cec0730dcbfa5a34d8b7823e225b84cedee0c
PatricSPires <patric.pires@outlook.com>
2020-02-01 13:11:08 -0300

    feat(button): create a button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+
+function ButtonComponent({ text }) {
+  return (
+    <>
+      <Button variant="contained" color="primary" type="button">
+        {text}
+      </Button>
+    </>
+  );
+}
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default ButtonComponent;
--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React from 'react';
+
+import Button from '../../components/Button';
 
 export default function Details() {
   return (
     <>
       <h1>Hello Details</h1>
+      <Button variant="contained" color="primary" text="Back" />
     </>
   );
 }
