0b3a8918aa9c3e72c6e61605df5e417d13da402a
PatricSPires <patric.pires@outlook.com>
2020-02-01 11:49:14 -0300

    feat(routes): create routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
+import Routes from "./routes";
 
 function App() {
-  return (
-    <div className="App">
-      <p>Faça algo bem legal aqui :D</p>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/details" component={Details} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
