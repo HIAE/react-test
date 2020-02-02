c822b12fadd0055e32ad41680543bc51c4825a5e
PatricSPires <patric.pires@outlook.com>
2020-02-01 23:17:01 -0300

    refactor(home): remove fixed

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,7 +7,6 @@ export default function Home() {
   return (
     <Container maxWidth="md">
       <Box
-        fixed
         display="flex"
         justifyContent="center"
         alignItems="center"
