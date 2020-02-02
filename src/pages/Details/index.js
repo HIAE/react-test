c9ae1b590db65f14b298c8df1ddcd2ae00662d6d
PatricSPires <patric.pires@outlook.com>
2020-02-01 21:18:54 -0300

    feat(details): add display flex

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 
 import Button from '../../components/Button';
 import Description from '../../components/Description';
@@ -7,13 +9,20 @@ import Chart from '../../components/Chart';
 
 export default function Details() {
   return (
-    <>
-      <h1>Hello Details</h1>
-      <NavLink to="/">
-        <Button variant="contained" color="primary" text="Back" />
-      </NavLink>
-      <Description text="Hello world, hello america." />
-      <Chart />
-    </>
+    <Container maxWidth="md">
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="flex-start"
+        flexDirection="column"
+        height="90vh"
+      >
+        <NavLink to="/">
+          <Button variant="contained" color="primary" text="Back" />
+        </NavLink>
+        <Description text="Hello world, hello america." />
+        <Chart />
+      </Box>
+    </Container>
   );
 }
