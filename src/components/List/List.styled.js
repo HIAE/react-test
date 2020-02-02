f257fd082add296338d33cbc7883c67435983955
PatricSPires <patric.pires@outlook.com>
2020-02-02 18:56:07 -0300

    refactor(list): use styled components

---
+++ src/components/List/List.styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ListComponent = styled.ul`
+  width: 400px;
+`;
+
+export const ListItem = styled.li`
+  color: #000;
+  font-family: 'Roboto', sans-serif;
+  list-style: none;
+  display: flex;
+  margin: 8px;
+`;
+
+export const ListItemText = styled.span`
+  margin-right: 10px;
+`;
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -2,38 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-
-import { makeStyles } from '@material-ui/core/styles';
-
 import Box from '@material-ui/core/Box';
-
-const useStyles = makeStyles({
-  list: {
-    width: '100%',
-    maxWidth: 400,
-    margin: '10px',
-  },
-});
+import { ListComponent, ListItem, ListItemText } from './List.styled';
 
 export default function CompanyList({ list }) {
-  const classes = useStyles();
   return (
     <>
-      <List className={classes.list}>
+      <ListComponent>
         {list.map(item => (
           <Link to={{ pathname: `/${item.symbol}/details` }}>
             <Box display="flex" justifyContent="flex-start">
-              <ListItem button>
-                <ListItemText primary={item.symbol} />
-                <ListItemText primary={item.name} />
+              <ListItem>
+                <ListItemText>{item.symbol}</ListItemText>
+                <ListItemText>{item.name}</ListItemText>
               </ListItem>
             </Box>
           </Link>
         ))}
-      </List>
+      </ListComponent>
     </>
   );
 }
