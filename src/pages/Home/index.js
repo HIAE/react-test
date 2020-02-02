f0a2c96daf834981ecf66dd4e3bcb774d2c2f556
PatricSPires <patric.pires@outlook.com>
2020-02-01 21:22:12 -0300

    refactor(home): add container wrapper

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import Search from '../../components/Search';
 
 export default function Home() {
   return (
-    <Box
-      fixed
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-      height="90vh"
-    >
-      <h1>Hello Home</h1>
-      <Search />
-    </Box>
+    <Container maxWidth="md">
+      <Box
+        fixed
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        height="90vh"
+      >
+        <h1>Hello Home</h1>
+        <Search />
+      </Box>
+    </Container>
   );
 }
