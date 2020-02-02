c7f56a87f2b72e19d69f47b01208d89355786049
PatricSPires <patric.pires@outlook.com>
2020-02-01 21:06:27 -0300

    feat(home): add display flex

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
 import Search from '../../components/Search';
 
 export default function Home() {
   return (
-    <>
+    <Box
+      fixed
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      height="90vh"
+    >
       <h1>Hello Home</h1>
       <Search />
-    </>
+    </Box>
   );
 }
