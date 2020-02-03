e1ad1810c4aaa73572177c91b0fc938441dd0118
PatricSPires <patric.pires@outlook.com>
2020-02-03 00:56:11 -0300

    refactor(search): add focus style

--- src/components/Search/Search.styled.js
+++ src/components/Search/Search.styled.js
@@ -5,6 +5,13 @@ const SearchComponent = styled.input`
   height: 40px;
   border: none;
   padding: 16px;
+  border-radius: 26px;
+  transition: 0.2s;
+
+  &:focus {
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 4px 4px;
+    transition: 0.2s;
+  }
 `;
 
 export default SearchComponent;
