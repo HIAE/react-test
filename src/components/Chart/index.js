4a5a12a49e113a69811b77bb5c3b1bbb0f724ef3
PatricSPires <patric.pires@outlook.com>
2020-02-05 02:23:31 -0300

    refactor(chart): remove cartesian grid and add line stoke

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -5,13 +5,13 @@ import {
   XAxis,
   YAxis,
   Legend,
-  CartesianGrid,
+  // CartesianGrid,
   Tooltip,
 } from 'recharts';
 
 export default function Chart({ prices }) {
-  const initialDate = '2019-12-31';
-  const finalDate = '2020-02-01';
+  const initialDate = '2020-01-22';
+  const finalDate = '2020-02-04';
 
   const informations = Object.keys(prices).map(key => ({
     date: key,
@@ -28,13 +28,13 @@ export default function Chart({ prices }) {
 
   return (
     <LineChart width={900} height={400} data={dataByDate}>
-      <CartesianGrid strokeDasharray="1" />
+      {/* <CartesianGrid strokeDasharray="1" /> */}
       <XAxis dataKey="date" />
       <YAxis datakey="close" domain={[dataByDate.open, dataByDate.close]} />
       <Tooltip />
       <Legend />
-      <Line dataKey="open" fill="peru" />
-      <Line dataKey="close" fill="#8884d8" />
+      <Line dataKey="open" fill="red" strokeWidth={2} type="monotone" />
+      <Line dataKey="close" fill="#8884d8" strokeWidth={3} type="monotone" />
     </LineChart>
   );
 }
