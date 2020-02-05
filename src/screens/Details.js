3292ab09dd7a804bc68668bee2c6af19a9ef0db6
Eduardo Cerqueira <eduardo@blanko.be>
2020-02-05 17:15:47 -0300

    memoize details

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useCallback, useMemo } from 'react'
 
 import {
     Container,
@@ -22,63 +22,90 @@ import { useHistory } from "react-router-dom"
 
 import ModalMessage from '../components/ModalMessage'
 
+import {
+    SET_MODAL
+} from '../redux/actions/actionTypes'
+
+import { 
+    useDispatch 
+} from 'react-redux'
+
 function Details(props) {
 
+    const dispatch = useDispatch()
     const { match: { params: { symbol } } } = props
     const [isLoading, setIsLoading] = useState(true)
     const [daily, setDaily] = useState(null)
-    const [modalInfo, setModalInfo] = useState({
-        open: false,
-    })
     const history = useHistory()
-    const isFirstRun = useRef(true)
+
+
+    const sendError = useCallback(message => {
+        dispatch({ 
+            type: SET_MODAL,
+            modal: {
+                open: true,
+                handleClose: () => {
+                    dispatch({
+                        type: SET_MODAL,
+                        modal: {
+                            open: false
+                        }
+                    })
+                    history.push('/')
+                },
+                title: 'Ops...',
+                description: message,
+                txtBtn: 'Ok!'
+            }
+        })
+    }, [dispatch, history])
 
     useEffect(() => {
         document.title = `${symbol} - Details`
         const makeGetDaily = async() => {
-
             try {
                 const response = await getDaily(symbol)
 
                 if (response.Note || response["Error Message"]) {
-                    makeSetModalInfo(response.Note || response["Error Message"])
+                    sendError(response.Note || response["Error Message"])
                     setIsLoading(false)
                 } else {
                     setDaily(response)
                     setIsLoading(false)
                 }
             } catch (error) {
-                makeSetModalInfo('Something unexpected happened, try again later!')
+                sendError('Something unexpected happened, try again later!')
                 setIsLoading(false)
                 console.error(error)
             }
         }
 
-        const makeSetModalInfo = msg => {
-            setModalInfo({
-                modal: {
-                  title: 'Ops...',
-                  description: msg,
-                  txtBtn: 'Ok!'
-                }
-            })
-        }
-
         makeGetDaily()
-    }, [symbol])
+    }, [symbol, sendError])
 
-    useEffect(() => {
-        if (isFirstRun.current) {
-            isFirstRun.current = false;
-            return
-        } else if(!modalInfo.open) {
-            setModalInfo({
-                ...modalInfo,
-                open: true,
-                handleClose: () => history.push('/'),
-            })
-        }
-    }, [modalInfo, history])
+    const useLoading = useMemo(() => {
+        return isLoading && 
+        <CircularProgress size='5rem' color={'primary'} />
+    }, [isLoading])
+
+    const useDaily = useMemo(() => {
+        return daily != null &&
+        <>  
+            <Grid xs={12} item>
+                <h1>
+                    {daily["Meta Data"]["2. Symbol"]}
+                </h1>
+                <p>
+                    <b>Last Refreshed: </b>
+                    {daily["Meta Data"]["3. Last Refreshed"].replace(/-/g, '/')}
+                </p>
+                <BackBtn />
+            </Grid>
+            <Grid xs={12} item>
+                <Chart daily={daily["Time Series (Daily)"]} />
+            </Grid>
+        </>
+    }, [daily])
 
     return(
         <Container>
@@ -87,33 +114,10 @@ function Details(props) {
                 justify="center"
                 container
             >
-                {isLoading && 
-                    <CircularProgress size='5rem' color={'primary'} />}
-
-                {!isLoading &&
-                    daily != null &&
-                        <>  
-                            <Grid xs={12} item>
-                                <h1>
-                                    {daily["Meta Data"]["2. Symbol"]}
-                                </h1>
-                                <p>
-                                    <b>Last Refreshed: </b>
-                                    {daily["Meta Data"]["3. Last Refreshed"].replace(/-/g, '/')}
-                                </p>
-                                <BackBtn />
-                            </Grid>
-                            <Grid xs={12} item>
-                                <Chart daily={daily["Time Series (Daily)"]} />
-                            </Grid>
-                        </>}
+                {useLoading}
+                {useDaily}
             </GridContainerDetails>
-            {modalInfo.open && 
-                <ModalMessage 
-                    open={modalInfo.open}
-                    handleClose={modalInfo.handleClose}
-                    modal={modalInfo.modal}
-                />}
+            <ModalMessage />
         </Container>
     )
 }
