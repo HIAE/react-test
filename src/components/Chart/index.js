7baca14c6b2c9188d17db2e2fe29e8e904fbd33b
PatricSPires <patric.pires@outlook.com>
2020-02-05 01:42:06 -0300

    refactor(chart): show bars based on initial and final dates

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -10,13 +10,23 @@ import {
 } from 'recharts';
 
 export default function Chart({ prices }) {
+  const initialDate = '2019-12-31';
+  const finalDate = '2020-02-01';
+
   const informations = Object.keys(prices).map(key => ({
     date: key,
     close: prices[key].close,
   }));
 
+  const dataByDate = informations.filter(item => {
+    return item.date >= initialDate && item.date <= finalDate;
+  });
+
+  console.log(informations);
+  console.log(dataByDate);
+
   return (
-    <BarChart width={900} height={400} data={informations}>
+    <BarChart width={900} height={400} data={dataByDate}>
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="date" />
       <YAxis datakey="close" />
