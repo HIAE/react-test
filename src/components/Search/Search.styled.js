69d12645772ca1ee5b915896d62c4439f03e7d71
PatricSPires <patric.pires@outlook.com>
2020-02-06 18:54:45 -0300

    refactor(search): change style and font size

--- src/components/Search/Search.styled.js
+++ src/components/Search/Search.styled.js
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 
 const SearchComponent = styled.input`
-  width: 300px;
+  width: 400px;
   height: 40px;
   border: none;
-  padding: 16px;
+  padding: 8px 16px;
   border-radius: 26px;
   transition: 0.2s;
+  margin-bottom: 32px;
+  font-size: 18px;
 
   &:focus {
     box-shadow: rgba(0, 0, 0, 0.25) 0px 4px 4px;
