55e94aa2cc809f6de99ddea56408b47eb058156f
PatricSPires <patric.pires@outlook.com>
2020-02-06 18:21:02 -0300

    refactor(title): change font size

--- src/components/Title/Title.styled.js
+++ src/components/Title/Title.styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { color } from '../../shared/variables';
 
 const TitleComponent = styled.h1`
-  font-size: 32px;
+  font-size: 34px;
   margin-bottom: 30px;
   color: ${color.catskill_white};
 `;
