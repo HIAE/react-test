a2b2365fd5adc05250742e913f71f2ef440df463
PatricSPires <patric.pires@outlook.com>
2020-02-07 01:51:37 -0300

    refactor(chart): remove unused variables

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -2,22 +2,19 @@ import React from 'react';
 import { LineChart, Line, XAxis, YAxis, Legend, Tooltip } from 'recharts';
 
 export default function Chart({ prices, today, endDate }) {
-  const todayDate = today;
-  console.log(endDate, today);
-
   const informations = Object.keys(prices).map(key => ({
     date: key,
     close: prices[key].close,
   }));
 
   const dataByDate = informations.filter(item => {
-    return item.date >= endDate && item.date <= todayDate;
+    return item.date >= endDate && item.date <= today;
   });
 
   return (
     <LineChart width={900} height={400} data={dataByDate}>
       <XAxis dataKey="date" />
-      <YAxis datakey="close" domain={[dataByDate.open, dataByDate.close]} />
+      <YAxis datakey="close" domain={[endDate, today]} />
       <Tooltip />
       <Legend />
       <Line
