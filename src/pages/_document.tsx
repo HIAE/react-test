7b5ec31b26e0f6ffe7b065ac6de7dd9ebcf1701c
TheHumphrey <fernandob.delima@outlook.com>
2022-12-05 19:35:34 -0300

    feat: install styled components and material-ui
    - config styled components compiler on next.config.js.
    - remove css import on "_app.tsx" file.
    - add link on _document.tsx for "material-ui".

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
